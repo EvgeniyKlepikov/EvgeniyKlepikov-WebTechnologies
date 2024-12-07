dd028a4ab64d5007d5e1e480339727d067cdbbe2
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-07 20:05:13 +0300

    Добавьте файлы проекта.

---
+++ KLEPIKOV30323WEB/KLEPIKOV30323WEB.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KLEPIKOV30323WEB.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
