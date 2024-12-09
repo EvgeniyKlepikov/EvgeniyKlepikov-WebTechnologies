89872f78d2ec1af716f6bc5aad4d319d08db6682
EvgeniyKlepikov <e_klepikov@mail.ru>
2024-12-10 00:35:45 +0300

    Лабораторная работа 2.

---
+++ KLEPIKOV30323WEB/KLEPIKOV30323WEB.UI/ViewComponents/CartViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KLEPIKOV30323WEB.UI.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
